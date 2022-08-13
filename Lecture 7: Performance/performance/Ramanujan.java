25dfbbf753b20ab8de6fc31ab177f70b49a21c7a
Kaki4427 <zhou.shijia@campus.lmu.de>
2022-08-13 23:46:36 +0200

    Week 7 Performance - Q2

---
+++ Lecture 7: Performance/performance/Ramanujan.java
@@ -0,0 +1,23 @@
+public class Ramanujan {
+
+    // Is n a Ramanujan number?
+    public static boolean isRamanujan(long n){
+        int count = 0;
+
+        for (long i = 1; Math.pow(i, 3) < n/2; i++) {
+            for (long j = i+1; Math.pow(j, 3) < n; j++) {
+                if (Math.pow(i, 3)+Math.pow(j, 3) == n) count++;
+                if (count > 1) return true;
+            }
+        }
+        return false;
+    }
+
+
+    // Takes a long integer command-line arguments n and prints true if
+    // n is a Ramanujan number, and false otherwise.
+    public static void main(String[] args) {
+        long n = Long.parseLong(args[0]);
+        StdOut.print(isRamanujan(n));
+    }
+}
